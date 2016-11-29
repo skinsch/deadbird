518260145c7c071842954a25981154c74717f18e
Keith Armstrong <keith1015@gmail.com>
2016-11-28 22:50:52 -0600

    Add all option to leaderboards table

--- public/js/leaderboards.js
+++ public/js/leaderboards.js
@@ -1,6 +1,7 @@
 $(() => {
   $('#leaderboard').DataTable( {
-    "order": [[ 1, "desc" ]]
+    "order": [[ 1, "desc" ]],
+    "lengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ]
   });
   $('#leaderboard').show();
 });
