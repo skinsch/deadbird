94e64a3f0d9590d736a0bdbae94f0fa909c1e6a5
Keith Armstrong <keith1015@gmail.com>
2017-01-15 15:18:11 -0600

    Convert refetcher to queue

--- refetcher.js
+++ refetcher.js
@@ -18,27 +18,55 @@ const db     = require('./models/db');
 const Tweet  = require('./models/tweet');
 
 let tweets = [];
+let cur    = 0;
+let fails  = 0;
+let q = async.queue((tweet, cb) => {
+  Tweet.readd(tweet).then(result => {
+    if (result === 'fail') {
+      fails++;
+      q.push(tweet);
+    }
+    cur++;
+    if (tty) {
+      charm.left(255);
+      charm.erase('line');
+      charm.write(`${cur} / ${tweets.length}+${fails} | ${Math.floor(cur/(tweets.length+fails)*100)} | ${tweets.length+fails-cur} | ${tweet.handlename} | https://twitter.com/${tweet.handlename}/status/${tweet.tweetid}`);
+    } else {
+      process.stdout.write(JSON.stringify({status: `${cur} / ${tweets.length}+${fails}`, percent: Math.floor(cur/(tweets.length+fails)*100), remaining: tweets.length+fails-cur, user: tweet.handlename, url: `https://twitter.com/${tweet.handlename}/status/${tweet.tweetid}`}));
+    }
+
+    cb();
+  });
+}, 15);
+q.pause();
+q.drain = () => {
+  if (tty) {
+    charm.down(1);
+    charm.cursor(true);
+    console.log(`\nFinished snapshotting tweets`);
+  } else {
+    process.stdout.write(JSON.stringify({done: true, text: `Finished snapshotting tweets`}));
+  }
+  process.exit();
+};
 
 async.series([
   cb => {
+    db.connection.query('UPDATE tweets SET checking = 0', (err, results) => {
+      console.log(`Reset checking flag for ${results.changedRows} tweets`);
+      cb();
+    });
+  },
+  cb => {
     db.connection.query('SELECT t.*, h.handle AS handlename FROM `tweets` t INNER JOIN `handles` h ON (t.handle=h.id) WHERE deleteDate IS NULL AND now() > checkDate AND checks < 7', (err, results) => {
       tweets = results;
+      tweets.forEach(tweet => {
+        q.push(tweet);
+      });
       cb();
     });
   }
 ], () => {
-  main();
+  if (q.length() === 0) q.drain();
+  else q.resume();
 });
-
-function main() {
-  let cur = 0;
-  console.log(tweets.length);
-  async.eachLimit(tweets, 15, (tweet, cb) => {
-    Tweet.readd(tweet).then(result => {
-      console.log(`${++cur}/${tweets.length}`);
-      cb();
-    });
-  }, () => {
-    process.exit();
-  });
-}
